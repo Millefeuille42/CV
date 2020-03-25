62929cd4d95fcf9eaa7513977edea50c96ce192b
millefeuille <mlabouri@stduent.42.fr>
2020-03-25 14:21:38 +0100

    V1

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const fs = require('fs');
+var device = require('express-device');
+
+var router = express.Router();
+
+router.use(device.capture());
+router.get('/', function(req, res, next) {
+
+    var data = JSON.parse(fs.readFileSync
+    ("./data.json" , "utf-8"));
+
+    console.log(req.device.type.toUpperCase());
+
+    res.render('index',
+        {
+            title: 'Mathieu Labourier',
+            contactList: data["contactList"],
+            scholarList: data["scholarList"],
+            achievList: data["achievList"],
+            projectList: data["projectList"],
+            TskillFList: data["aboutList"]["TskillFList"],
+            SskillFList: data["aboutList"]["SskillFList"],
+            spareTimeActivities: data["aboutList"]["spareTimeActivities"],
+            languages: data["aboutList"]["languages"],
+            mobile: req.device.type.toUpperCase()
+    });
+});
+
+module.exports = router;

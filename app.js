e85e1d95c46e6cb095aac3493209c9ab13e0bda0
millefeuille <mlabouri@stduent.42.fr>
2020-03-25 14:26:31 +0100

    V1.1 Added Project

--- app.js
+++ app.js
@@ -18,11 +18,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/cv', indexRouter);
+app.use('/', indexRouter);
 
 app.use(function(req, res, next){
   res.setHeader('Content-Type', 'text/plain');
-  res.redirect('/cv');
+  res.redirect('/');
 });
 
 // error handler
